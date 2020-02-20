2d6dde3851945ac8ab12b1b85f78a3d05bd26952
Marco Turchetto <marco.turchetto.mt@gmail.com>
2020-02-20 21:25:24 +0100

    add counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import React from "react";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div>
       <h1>My App</h1>
+      <Counter label="Count" />
+      <Counter label="Clicks" />
+      <Counter label="Interactions" />
     </div>
   );
 }
